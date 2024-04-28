fc523c57ebe35d428682655d9ada44bab5ba9c71
devmazatlanic <164423179+devmazatlanic@users.noreply.github.com>
2024-04-27 18:55:17 -0700

    agregando gitignore

--- databases/config.js
+++ databases/config.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'mic.red-tel.net',
+    // host: 'mic.red-tel.net',
+    host: '192.168.80.117',
     user: 'db_crmoperation_',
     password: '0p3r_cRm*1_',
     database: 'db_crmtesting'
