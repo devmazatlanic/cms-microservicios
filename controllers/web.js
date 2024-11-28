e2e5b0c43caae7c533d2e6eb3e952e7dad72edde
Antonino Rodriguez <antoninorodriguez@solemti.com>
2024-11-27 21:16:35 -0700

    actualizaciones de rutas

--- controllers/web.js
+++ controllers/web.js
@@ -1,11 +1,11 @@
 const { request, response } = require('express');
-const { evenots_web } = require('../models/eventos');
+const { evenots_web_hoy } = require('../models/eventos');
 // const { enviarCorreo } = require('../config/mail');
 
 
-const event_list = async (request, response) => {
+const event_list_hoy = async (request, response) => {
     // MOSTRAREMOS LISTA DE EVENTOS ACTIVOS
-    const eventos = await evenots_web();
+    const eventos = await evenots_web_hoy();
 
     if (!eventos || eventos.length === 0) {
         // Manejo de caso en que no se obtengan resultados
@@ -27,10 +27,36 @@ const event_list = async (request, response) => {
             next: true
         });
     }
+};
 
+const event_list_proximos = async (request, response) => {
+    // MOSTRAREMOS LISTA DE EVENTOS ACTIVOS
+    const eventos = await evenots_web_hoy();
+
+    if (!eventos || eventos.length === 0) {
+        // Manejo de caso en que no se obtengan resultados
+        return response.status(200).json({
+            next: false,
+            message: "NO SE ENCONTRARON EVENTOS."
+        });
+    } else {
+        // EXTRAEMOS LOS DATOS RELEVANTES DE LA RESPUESTA DE LA BASE DE DATOS
+        const _eventos = eventos.map(_element => ({
+            evento: _element.evento,
+            fecha: _element.fecha,
+            image: `data:image/png;base64,${_element.image}`
+        }));
+
+
+        return response.status(200).json({
+            data: _eventos,
+            next: true
+        });
+    }
 };
 
 
 module.exports = {
-    event_list
+    event_list_hoy,
+    event_list_proximos
 }
--- routes/web.js
+++ routes/web.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { event_list } = require('../controllers/web');
+const { event_list_hoy, event_list_proximos } = require('../controllers/web');
 
 const router = Router();
 
-router.get('/events', event_list);
+router.get('/events/today', event_list_hoy);
+router.get('/events/upcoming', event_list_proximos);
 
 
 module.exports = router;
