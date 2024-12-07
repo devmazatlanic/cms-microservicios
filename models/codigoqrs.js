7b040825787f7cced9f9bf9c768d86c81d1e9e0d
Antonino Rodriguez <janto_sega5@hotmil.com>
2024-12-07 11:41:42 -0700

    actualizacion del modelo

--- models/codigoqrs.js
+++ models/codigoqrs.js
@@ -7,6 +7,33 @@ const addMapeoQr = async (_data) => {
             throw new Error('Faltan datos requeridos para insertar en la base de datos.');
         }
 
+        if (typeof _data.ip !== 'string' || _data.qr == '0') {
+            throw new Error('"qr" debe ser un número entero mayor a 0.');
+        }
+
+        if (typeof _data.ip !== 'string' || _data.ip.trim() === '') {
+            throw new Error('"ip" debe ser una cadena de texto no vacía.');
+        }
+
+        const existingRecord = await new Promise((resolve, reject) => {
+            const sql = `SELECT id FROM tcr_mapeo_qr WHERE ip = ?`;
+            connection.query(sql, [_data.ip], (error, results) => {
+                if (error) {
+                    reject(new Error(`Error al verificar la IP: ${error.message}`));
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+
+        if (existingRecord.length > 0) {
+            // La IP ya existe, retorna success sin intentar registrar
+            return {
+                success: true,
+                message: 'La IP ya existe en la base de datos.'
+            };
+        }
+
         // Realizar la consulta INSERT usando parámetros preparados
         const insertResult = await new Promise((resolve, reject) => {
             const sql = `
