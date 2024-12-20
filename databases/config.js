0f730ba76b444ac5c6f8443d45e93bee27c54dd0
cjosue2g <cjosue2g.fimaz@gmail.com>
2024-12-20 12:45:45 -0700

    feature: flujo pantallas playlist

--- databases/config.js
+++ databases/config.js
@@ -2,12 +2,13 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     connectionLimit: 10, // Límite de conexiones simultáneas
-    // host: 'mic.red-tel.net',
-    host: '192.168.80.117',
+    host: 'mic.red-tel.net',
+    // host: '192.168.80.117',
     user: 'db_crmoperation_',
     password: '0p3r_cRm*1_',
     // database: 'db_crmtesting'
-    database: 'db_crmmazatlanic'
+    database: 'db_crmmazatlanic',
+    port: 3307
 });
 
 module.exports = {
