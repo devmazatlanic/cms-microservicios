73f9257149ab2731b5fb6b489ec62bc76a6e2c29
Antonino Rodriguez <janto_sega5@hotmil.com>
2024-12-20 12:49:02 -0700

    actualizando

--- databases/config.js
+++ databases/config.js
@@ -3,10 +3,9 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     connectionLimit: 10, // Límite de conexiones simultáneas
     // host: 'mic.red-tel.net',
-    host: '192.168.80.117',
+    host: '192.168.80.241',
     user: 'db_crmoperation_',
     password: '0p3r_cRm*1_',
-    // database: 'db_crmtesting'
     database: 'db_crmmazatlanic'
 });
 
